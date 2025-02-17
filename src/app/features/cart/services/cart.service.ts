e8542e72734c8414ae0fcac31c0017fa4326289a
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-02-17 02:03:40 +0200

    v0.3

--- src/app/features/cart/services/cart.service.ts
+++ src/app/features/cart/services/cart.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../../Authentication/services/auth.service';
+import { env } from '../../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+
+  addProduct(productId: string) {
+    this.http.post(env.BASE_URL + 'cart', {
+      productId
+    }, {
+      headers: {
+        token: this.authService.getToken()!
+      }
+    })
+  }
+
+  updateProductQuantity(productId: string, count: number) {
+    this.http.put(env.BASE_URL + `cart/${productId}`, {
+      count
+    }, {
+      headers: {
+        token: this.authService.getToken()!
+      }
+    })
+  }
+
+  getUserCart() {
+    this.http.get(env.BASE_URL + 'cart', {
+      headers: {
+        token: this.authService.getToken()!
+      }
+    })
+  }
+  clearCart() {
+    this.http.delete(env.BASE_URL + 'cart', {
+      headers: {
+        token: this.authService.getToken()!
+      }
+    })
+  }
 }
